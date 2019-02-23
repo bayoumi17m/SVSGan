31ad76761b63d2dc0bf781a7874efc4f6b6e2215
Magd Bayoumi <git>
2019-02-23 18:37:59 -0500

    Adding dataset loader

--- util.py
+++ util.py
@@ -2,9 +2,30 @@ import os
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from random import choice
 
 class DSD100Dataset(Dataset):
     """DOcstring for DSD100 Dataset"""
 
-    def __init__(self, root_dir, length):
+    def __init__(self, root_dir):
         """Docstring for the Dataset object"""
+        length = len(list(filter(lambda x: x[-3:] == ".wav", os.listdir("."))))
+        self.data = np.empty(length)
+        i = 0
+        for filename in os.listdir(root_dir):
+            if filename.endswith(".wav"):
+                rate, data = scipy.io.wavefile.read(filename)
+                f, t, Sxx = scipy.signal.spectrogram(data,rate)
+
+                self.data[i] = (rate,Sxx)
+
+            else:
+                pass
+
+        def __len__(self):
+            return self.data.shape[0]
+
+
+        def __getitem__(self, _):
+            return np.random.choice(self.data)[1]
+
