5a6b39daa3adde05a2e12594ad57520ef64cb1bc
Xiuyu Li <1183135553@qq.com>
2019-03-01 02:31:02 -0500

    Move args to utils and add more args for abstraction

--- util.py
+++ util.py
@@ -3,6 +3,35 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 from random import choice
+import numpy as np
+import scipy
+
+def get_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    #parser.add_argument('--dataset', type=str, default='mnist', help='Load a previous dataset')
+    parser.add_argument('--dataroot', type=str, default='./data/', help='path to dataset')
+    parser.add_argument('--resume', type=str, default=None, help='File to resume')
+    parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
+    parser.add_argument('--batch_size', type=int, default=128, help='input batch size')
+    parser.add_argument('--lrG', type=float, default=0.001,
+            help='learning rate for generator, default=0.001')
+    parser.add_argument('--lrD', type=float, default=0.001,
+            help='learning rate for Discriminator, default=0.001')
+    parser.add_argument('--Gbeta1', type=float, default=0.5, help='Generator beta1 for adam. default=0.5')
+    parser.add_argument('--Gbeta2', type=float, default=0.999, help='Generator beta2 for adam. default=0.999')
+    parser.add_argument('--Dbeta1', type=float, default=0.5, help='Discriminator beta1 for adam. default=0.5')
+    parser.add_argument('--Dbeta2', type=float, default=0.999, help='Discriminator beta2 for adam. default=0.999')
+    parser.add_argument('--cuda', action='store_true', help='enables cuda')
+    parser.add_argument('--seed', default=100, type=int, help='Random seed.')
+    parser.add_argument('--load', action="store_true", help='load dataset')
+    parser.add_argument('--ngf', type=int, default=1024, help='number of features in generator')
+    parser.add_argument('--ndf', type=int, default=513, help='number of features in discriminator')
+    parser.add_argument('--N_FFT', type=int, default=513, help='size of the input spectra of the generator')
+    parser.add_argument('--inD', type=int, default=1539, help='size of the input features of the discriminator')
+    args = parser.parse_args()
+
+    return args
 
 class DSD100Dataset(Dataset):
     """DOcstring for DSD100 Dataset"""
