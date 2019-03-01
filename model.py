1083e0e01c332488967b922b62da482faf31b7d4
Xiuyu Li <1183135553@qq.com>
2019-03-01 02:32:28 -0500

    Modify the structure of the Generator and the Discriminator

--- model.py
+++ model.py
@@ -6,21 +6,21 @@ import torch.optim as optim
 from torch.autograd import Variable
 import pickle
 import os
+import utils
 
 class Generator(nn.Module):
     """Docstring for Generator"""
-    def __init__(self,N_FFT,hidden_nodes_G):
+    def __init__(self,args):
+        super(Generator, self).__init__()
         """Docstring for Init of Generator"""
         # We take in a vector of size 1024?
-        self.fc1 = nn.Linear(N_FFT,hidden_nodes_G)
-        self.fc2 = nn.Linear(hidden_nodes_G,hidden_nodes_G)
-        self.fc3 = nn.Linear(hidden_nodes_G,hidden_nodes_G)
-        self.fc4 = nn.Linear(hidden_nodes_G,N_FFT)
-
-        self.bn1 = nn.BatchNorm(batch_size)
-        self.bn2 = nn.BatchNorm(batch_size)
-        self.bn3 = nn.BatchNorm(batch_size)
-        self.bn4 = nn.BatchNorm(batch_size)
+        self.fc1 = nn.Linear(args.N_FFT,args.ngf)
+        self.bn1 = nn.BatchNorm1d(args.ngf)
+        self.fc2 = nn.Linear(args.ngf,args.ngf)
+        self.bn2 = nn.BatchNorm1d(args.ngf)
+        self.fc3 = nn.Linear(args.ngf,args.ngf)
+        self.bn3 = nn.BatchNorm1d(args.ngf)
+        self.fc4 = nn.Linear(args.ngf,args.N_FFT)
 
 
     def forward(self,z):
@@ -29,7 +29,7 @@ class Generator(nn.Module):
         x = F.relu(self.bn1(self.fc1(z)))
         x = F.relu(self.bn2(self.fc2(x)))
         x = F.relu(self.bn3(self.fc3(x)))
-        x = F.relu(self.bn4(self.fc4(x)))
+        x = F.tanh(self.fc4(x))
 
         # x1 = F.sigmoid(self.fc1(z1))
         # x1 = F.sigmoid(self.fc2(x1))
@@ -53,27 +53,44 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
     """Docstring for Discriminator"""
 
-    def __init__(self,in_size,hidden_nodes_D,batch_size):
+    def __init__(self,args):
+        super(Discriminator, self).__init__()
         """Docstring for Init of Discriminator"""
-        self.fc1 = Linear(in_size,hidden_nodes_D)
-        self.fc2 = Linear(hidden_nodes_D,hidden_nodes_D)
-        self.fc3 = Linear(hidden_nodes_D,hidden_nodes_D)
-        self.fc4 = Linear(hidden_nodes_D,1)
-        
-        self.bn1 = nn.BatchNorm(batch_size)
-        self.bn2 = nn.BatchNorm(batch_size)
-        self.bn3 = nn.BatchNorm(batch_size)
-        self.bn4 = nn.BatchNorm(batch_size)
+        self.fc1 = nn.Linear(args.inD, args.ndf)
+        self.fc2 = nn.Linear(args.ndf, args.ndf)
+        self.bn2 = nn.BatchNorm1d(args.ndf)
+        self.fc3 = nn.Linear(args.ndf, args.ndf)
+        self.bn3 = nn.BatchNorm1d(args.ndf)
+        self.fc4 = nn.Linear(args.ndf, 1)
 
+    def forward(self,y1,y2,z):
+        """forward function that takes two sources from the generator and the mixture"""
+        x = torch.cat([y1, y2, z], 1)
+        x = F.leaky_relu(self.fc1(x), 0.2)
+        x = F.leaky_relu(self.bn2(self.fc2(x)), 0.2)
+        x = F.leaky_relu(self.bn3(self.fc3(x)), 0.2)
+        x = F.sigmoid(self.fc4(x))
 
-
-    def forward(self,z):
-        """Docstring for forward function"""
-        x = F.relu(self.bn1(self.fc1(z)))
-        x = F.relu(self.bn2(self.fc2(x)))
-        x = F.relu(self.bn3(self.fc3(x)))
-        x = F.sigmoid(self.bn4(self.fc4(x)))
         return x
+    #     self.fc1 = Linear(in_size,hidden_nodes_D)
+    #     self.fc2 = Linear(hidden_nodes_D,hidden_nodes_D)
+    #     self.fc3 = Linear(hidden_nodes_D,hidden_nodes_D)
+    #     self.fc4 = Linear(hidden_nodes_D,1)
+        
+    #     self.bn1 = nn.BatchNorm(batch_size)
+    #     self.bn2 = nn.BatchNorm(batch_size)
+    #     self.bn3 = nn.BatchNorm(batch_size)
+    #     self.bn4 = nn.BatchNorm(batch_size)
+
+
+
+    # def forward(self,z):
+    #     """Docstring for forward function"""
+    #     x = F.relu(self.bn1(self.fc1(z)))
+    #     x = F.relu(self.bn2(self.fc2(x)))
+    #     x = F.relu(self.bn3(self.fc3(x)))
+    #     x = F.sigmoid(self.bn4(self.fc4(x)))
+    #     return x
 
 
 
