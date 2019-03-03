8272fbf89204967a0fe59cfd9397f4c150dcb984
Xiuyu Li <1183135553@qq.com>
2019-03-03 18:25:01 -0500

    Implement DSD100 loading methods

--- utils.py
+++ utils.py
@@ -7,6 +7,8 @@ import numpy as np
 import scipy
 from scipy import signal
 import scipy.io.wavfile
+from scipy.io.wavfile import read
+from pydub import AudioSegment
 
 def get_args():
     import argparse
@@ -93,3 +95,54 @@ def get_loader(args):
 
     return data_loader
 
+
+dataset_paths = {
+    'mixtures': os.path.join('DSD100DatasetPath', 'Mixtures'),
+    'sources': os.path.join('DSD100DatasetPath', 'Sources')
+}
+
+
+def get_DSD_files(subset):
+    """Getting the files lists.
+
+    :param subset: The subset that we are interested in (i.e. training or testing).
+    :type subset: str
+    :return: The lists with the file paths of the files that we want to use.
+    :rtype: (list[str], list[str])
+    """
+    specific_dir = 'Dev' if subset == 'training' else 'Test'
+    mixtures_dir = os.path.join(dataset_paths['mixtures'], specific_dir)
+    sources_dir = os.path.join(dataset_paths['sources'], specific_dir)
+
+    mixtures_list = [os.path.join(mixtures_dir, file_path)
+                     for file_path in sorted(os.listdir(mixtures_dir))]
+
+    sources_list = [os.path.join(sources_dir, file_path)
+                    for file_path in sorted(os.listdir(sources_dir))]
+
+    return mixtures_list, sources_list
+
+def get_train_np(devs):
+    mix_train = [np.array(read(os.path.join(dev, "mixture.wav"))[1], dtype=float)  for dev in devs[0]]
+    vocals_train = [np.array(read(os.path.join(dev, "vocals.wav"))[1], dtype=float)  for dev in devs[1]]
+    bgm_train = []
+    for dev in devs[1]:
+        bass = AudioSegment.from_wav(os.path.join(dev, "bass.wav"))
+        drums = AudioSegment.from_wav(os.path.join(dev, "drums.wav"))
+        other = AudioSegment.from_wav(os.path.join(dev, "other.wav"))
+        bgm = bass.overlay(drums.overlay(other))
+        bgm_train.append(np.array(bgm.get_array_of_samples(), dtype=float))
+    return mix_train, vocals_train, bgm_train
+
+
+def get_test_np(tests):
+    mix_test = [np.array(read(os.path.join(test, "mixture.wav"))[1], dtype=float) for test in tests[0]]
+    vocals_test = [np.array(read(os.path.join(test, "vocals.wav"))[1], dtype=float)  for test in tests[1]]
+    bgm_test = []
+    for test in tests[1]:
+        bass = AudioSegment.from_wav(os.path.join(test, "bass.wav"))
+        drums = AudioSegment.from_wav(os.path.join(test, "drums.wav"))
+        other = AudioSegment.from_wav(os.path.join(test, "other.wav"))
+        bgm = bass.overlay(drums.overlay(other))
+        bgm_test.append(np.array(bgm.get_array_of_samples(), dtype=float))
+    return mix_test, vocals_test, bgm_test
