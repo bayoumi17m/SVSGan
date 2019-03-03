f5835a8d0991a83b016b99cb1e454fc456a0aeaf
Magd Bayoumi <git>
2019-03-03 17:34:04 -0500

    fixed preprocessing

--- preprocessData.py
+++ preprocessData.py
@@ -7,7 +7,7 @@ from torch.autograd import Variable
 import pickle
 import os
 
-import util
+import utils
 
 def parse_args():
     import argparse
@@ -17,5 +17,5 @@ def parse_args():
     parser.add_argument('--store_data', type=str, default='./data/', help='path to dataset')
 
 if __name__ == "__main__":
-    args = parse_arguments_Data()
-    util.DataSetCleaner(args.dataroot, args.store_data)
+    args = parse_args()
+    utils.DataSetCleaner(args.dataroot, args.store_data)
