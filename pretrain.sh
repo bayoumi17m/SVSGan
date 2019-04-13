93fb00f3a8f43a0a7401eccd31c0e96899cac901
q-hwang <qh53@cornell.edu>
2019-04-13 15:43:51 -0400

    Fix configuration

--- pretrain.sh
+++ pretrain.sh
@@ -1,1 +1,1 @@
-python pretrain.py --train_directory=../../SVSGan/data/ --val_directory=../../SVSGan/empty/ --test_directory=../../SVSGan/empty/ --log-step=3 --lrG=${1}
+python pretrain.py --train_directory=./data/ --val_directory=./empty/ --test_directory=./empty/ --log-step=3 --lrG=${1}
